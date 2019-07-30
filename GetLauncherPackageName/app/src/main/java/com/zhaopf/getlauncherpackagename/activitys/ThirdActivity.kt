b709f181c20316b796b786f4a5a8f928e4a82d7f
ZhaoPingFu <shuai.wang@appshahe.com>
2019-07-30 10:07:06 +0800

    通过隐士跳转协议跳转到metaapp

--- GetLauncherPackageName/app/src/main/java/com/zhaopf/getlauncherpackagename/activitys/ThirdActivity.kt
+++ GetLauncherPackageName/app/src/main/java/com/zhaopf/getlauncherpackagename/activitys/ThirdActivity.kt
@@ -1,12 +1,21 @@
 package com.zhaopf.getlauncherpackagename.activitys
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.zhaopf.getlauncherpackagename.R
+import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        btn_jump_to_metaapp.setOnClickListener {
+            val uri = Uri.parse("metaapp://233xyx/splash")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
