34078ab7f4a45805a1114c0be01512a72988905b
ZhaoPingFu <pingfu.zhao@appshahe.com>
2019-08-06 17:23:35 +0800

    第一个有bug 的版本

--- RobustDemo/app/src/main/java/com/meta/robustdemo/SecondActivity.java
+++ RobustDemo/app/src/main/java/com/meta/robustdemo/SecondActivity.java
@@ -2,7 +2,9 @@ package com.meta.robustdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -14,6 +16,14 @@ public class SecondActivity extends AppCompatActivity {
         Button btnSecondBug = findViewById(R.id.btn_second_bug);
 
         btnSecondBug.setText(getSecondText());
+
+        btnSecondBug.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondActivity.this, "second bug.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private String getSecondText() {
