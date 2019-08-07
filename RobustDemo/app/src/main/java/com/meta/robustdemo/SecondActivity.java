89ed43b2c6dd465e793f324184a798d4c1d676c1
ZhaoPingFu <pingfu.zhao@appshahe.com>
2019-08-07 09:47:55 +0800

    恢复bug，测试

--- RobustDemo/app/src/main/java/com/meta/robustdemo/SecondActivity.java
+++ RobustDemo/app/src/main/java/com/meta/robustdemo/SecondActivity.java
@@ -1,17 +1,11 @@
 package com.meta.robustdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.meituan.robust.patch.annotaion.Add;
-import com.meituan.robust.patch.annotaion.Modify;
 
 public class SecondActivity extends AppCompatActivity {
 
-    @Modify
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,38 +14,10 @@ public class SecondActivity extends AppCompatActivity {
         Button btnSecondBug = findViewById(R.id.btn_second_bug);
 
         // btnSecondBug.setText(getSecondText());
-        btnSecondBug.setText(getSecondText2());
-
-        btnSecondBug.setOnClickListener(new SecondActivityClickListener());
+        btnSecondBug.setText(getSecondText());
     }
 
     private String getSecondText() {
         return "second bug";
-    }
-
-    @Add
-    private String getSecondText2() {
-        StringBuilder sb = new StringBuilder();
-        for (String s : getStrArray()) {
-            sb.append(s);
-            sb.append(" ");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
-    }
-
-    @Add
-    public String[] getStrArray() {
-        return new String[]{"hello", "world"};
-    }
-
-    @Add
-    public static class SecondActivityClickListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(v.getContext(), "second bug fixed.", Toast.LENGTH_SHORT)
-                    .show();
-        }
     }
 }
