a66b09107349f3c3a2851a8d5b981f1be9ade522
zhaopf <1308108803@qq.com>
2019-08-25 11:12:12 +0800

    beta2版本有坑，升级需谨慎

--- TestMotionLayout/app/src/main/java/com/meta/testmotionlayout/circular_reveal_helper/CircularRevealHelper.kt
+++ TestMotionLayout/app/src/main/java/com/meta/testmotionlayout/circular_reveal_helper/CircularRevealHelper.kt
@@ -3,10 +3,10 @@ package com.meta.testmotionlayout.circular_reveal_helper
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
-import android.view.View
 import android.view.ViewAnimationUtils
 import androidx.constraintlayout.widget.ConstraintHelper
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.math.hypot
 
 class CircularRevealHelper : ConstraintHelper {
 
@@ -14,23 +14,33 @@ class CircularRevealHelper : ConstraintHelper {
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    constructor (context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor (context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
 
     override fun updatePostLayout(container: ConstraintLayout?) {
         super.updatePostLayout(container)
         container ?: return
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            return
-        }
-        for (view in getViews(container)) {
-            val animator = ViewAnimationUtils.createCircularReveal(
-                view,
-                view.width / 2,
-                view.height / 2,
-                0f,
-                Math.hypot((view.height / 2).toDouble(), (view.width / 2).toDouble()).toFloat()
-            )
-            animator.duration = 3000
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return
+        for (i in 0 until mCount) {
+            val view = container.getViewById(referencedIds[i])
+            val animator = if (i == 0) {
+                ViewAnimationUtils.createCircularReveal(
+                    view, 0, 0, 0f,
+                    hypot(view.height.toDouble(), view.width.toDouble()).toFloat()
+                )
+            } else {
+                ViewAnimationUtils.createCircularReveal(
+                    view,
+                    view.width / 2,
+                    view.height / 2,
+                    0f,
+                    hypot(view.height / 2.0, view.width / 2.0).toFloat()
+                )
+            }
+            animator.duration = 1500L
             animator.start()
         }
     }
