d167698755cfec2066d64ca0d22bf773c927ea41
zhaopf <1308108803@qq.com>
2019-08-30 21:48:22 +0800

    添加MotionLayoutActivity

---
+++ TestMotionLayout/app/src/main/java/com/meta/testmotionlayout/MotionLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.meta.testmotionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MotionLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_layout)
+    }
+}
