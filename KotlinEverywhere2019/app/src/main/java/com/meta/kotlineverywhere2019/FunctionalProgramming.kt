043a1264c6504d58222d8642a9fdf273f3a66df0
zhaopf <1308108803@qq.com>
2019-09-12 16:00:47 +0800

    修改补丁包

--- KotlinEverywhere2019/app/src/main/java/com/meta/kotlineverywhere2019/FunctionalProgramming.kt
+++ KotlinEverywhere2019/app/src/main/java/com/meta/kotlineverywhere2019/FunctionalProgramming.kt
@@ -13,3 +13,30 @@ fun sample(a: Int.(Float, Double) -> Long): Long = 3.a(1f, 2.0)
 fun main() = println(sample(function))
 
 fun main2() = println(sample(Int::sample))
+
+/**
+ * 成员引用
+ */
+
+
+class Test {
+    fun functionParam(a: Int) = println(a)
+}
+
+fun function1(a: Test.(Int) -> Unit) = Test().a(6)
+fun function2(a: (Int) -> Unit) = a(6)
+
+fun test() {
+    /**
+     * 使用成员引用，及时用类名引用成员函数（扩展函数）
+     * 此时 Test::functionParam 的类型为 Test.(Int) -> Unit
+     */
+    function1(Test::functionParam)
+
+    /**
+     * 使用绑定引用，即直接使用对象去引用成员函数（扩展函数）
+     * 此时 test::functionParam 的类型为 (Int) -> Unit
+     */
+    val test = Test()
+    function2(test::functionParam)
+}
