2cd733f69891be724c63fdcdc14ed6f022265ebd
zhaopingfu <zhaopingfu@jiemian.com>
2020-12-17 17:21:52 +0800

    【ViewBinding】Add viewBinding inline function.

--- TestLazyFragment/app/src/main/java/com/zhaopf/testlazyfragment/MainActivity.kt
+++ TestLazyFragment/app/src/main/java/com/zhaopf/testlazyfragment/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.zhaopf.testlazyfragment
 
+import android.app.Activity
+import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 import com.zhaopf.testlazyfragment.adapter.MainVpAdapter
 import com.zhaopf.testlazyfragment.databinding.ActivityMainBinding
 
@@ -10,10 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     private val mFullScreenObserver by lazy { FullScreenObserver(this) }
 
+    private val binding by lazy { inflate<ActivityMainBinding>() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         with(binding.vpAppMainTopVp) {
@@ -31,4 +37,15 @@ class MainActivity : AppCompatActivity() {
         // 沉浸式
         mFullScreenObserver.applyKitKatTranslucencyWithColor(Color.BLACK)
     }
+}
+
+inline fun <reified T : ViewBinding> Activity.inflate(): T = inflateBinding<T>(layoutInflater)
+
+inline fun <reified T : ViewBinding> Fragment.inflate(): T = inflateBinding(layoutInflater)
+
+inline fun <reified T : ViewBinding> Dialog.inflate(): T = inflateBinding<T>(layoutInflater)
+
+inline fun <reified T : ViewBinding> inflateBinding(layoutInflater: LayoutInflater): T {
+    val method = T::class.java.getMethod("inflate", LayoutInflater::class.java)
+    return method.invoke(null, layoutInflater) as T
 }
